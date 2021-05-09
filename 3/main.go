05ced8c1aa94b7c0d216271dbc48f353264241d3
lyz <lyz@gmail.com>
2021-05-09 11:16:00 +0800

    init2

--- 3/main.go
+++ 3/main.go
@@ -24,21 +24,30 @@ var (
 func main() {
 L:
 	for outstandingPrincipal > monthlyPay {
+		// 一年的还款周期
 		for i := 0; i < 12; i++ {
+			// 第一个还款月不算，每3个月还款数增加
 			if totalMonths != 0 && i%3 == 0 {
 				monthlyPay = monthlyPay * morePayRate
 			}
+			// 增加总还款数
 			totalPay += monthlyPay
+			// 增加每年累计还款数
 			annualPay += monthlyPay
+			// 增加总还款月
 			totalMonths++
+			// 如果当年累计还款数已经超过未还金额，说明已经还完
 			if annualPay >= outstandingPrincipal {
+				// 计算最后一个月还款数的偏差值
 				lastPayOffset = annualPay - outstandingPrincipal
 				break L
 			}
 		}
+		// 一年还完后将每年累计还款金额清零
 		annualPay = 0
+		// 重新计算未还金额
 		outstandingPrincipal = outstandingPrincipal*annualInterest + (outstandingPrincipal - annualPay)
 	}
 	log.Printf("It will take %d months", totalMonths)
-	log.Printf("It will pay %.2f in total", totalPay - lastPayOffset)
+	log.Printf("It will pay %.2f in total", totalPay-lastPayOffset)
 }
